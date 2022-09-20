1c3ebeb61b7fdadf7837a2b2f9702fa49d0a4b28
matheenshaik <pooja@LAPTOP-GH4T6A65>
2022-09-20 12:24:22 +0530

    index fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const route = require('./routes/route.js');
+ const route = require('./routes/route.js');
 const app = express();
 const mongoose = require ('mongoose')
 
@@ -8,14 +8,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("", {
+mongoose.connect("mongodb+srv://matheenahamad:9TNGWEhzUB0Ttemi@matheen.vtdepfw.mongodb.net/group57Database", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
 
-app.use('/', route);
+ app.use('/', route);
 
 app.listen(process.env.PORT || 3000, function () {
     console.log('Express app running on port ' + (process.env.PORT || 3000))
