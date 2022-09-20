f146f9e270a40814578857b76db3e61f005fb3a3
salman170 <salmanansari02120@gmail.com>
2022-09-20 13:47:13 +0530

    basic structures of api

---
+++ src/controllers/userController.js
@@ -0,0 +1,63 @@
+const userModel = require("../model/userModel")
+const jwt = require('jsonwebtoken')
+
+
+
+
+
+//<----------------------Create User API --------------------->
+//Tarun, Matheen 
+const createUser = async function (){
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//<---------------------- User Login API --------------------->
+//Awneesh 
+const userLogin = async function (){
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = {createUser, userLogin }
+
+
+
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,4 +1,13 @@
 const express = require("express")
 const router = express.Router();
+const {createUser ,userLogin} = require('../controllers/userController')
+
+
+
+router.post("/register", createUser)
+router.post('/login', userLogin)
+
+// router.get('/test-me')
+
 
   module.exports=router
